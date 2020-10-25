49b4d9e76df6e4139e11c36825a54851ebb2d33b
nDJay11 <ndjay11@github.com>
2020-10-25 20:54:07 +0900

    declare TNEF Attribute

--- winmailParser.py
+++ winmailParser.py
@@ -2,6 +2,11 @@ import sys
 import struct
 
 WINMAIL_SIGNATURE = 0x223e9f78
+ATTACHMENT_DATA = 0x800F
+ATTACHMENT_NAME = 0x8010
+ATTACHMENT_OFFSET = 0x9002
+ATTACHMENT_PROPS = 0x9005
+MESSAGE_PROPS = 0x9003
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
